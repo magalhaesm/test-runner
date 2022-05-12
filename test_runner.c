97595012dc755624864bae2a989615a9a84a1849
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-05-12 11:05:46 -0300

    refatoração

--- test_runner.c
+++ test_runner.c
@@ -1,47 +1,65 @@
 #include "test_runner.h"
 #include <stdio.h>
 
-t_test *g_curr_test;
+t_test	g_test;
 
 void test_assert(int condition, int line)
 {
-	g_curr_test->line = line;
 	if (!condition)
-		g_curr_test->state.failed = 1;
+	{
+		g_test.line = line;
+		g_test.failed = 1;
+		g_test.failures += 1;
+	}
 }
 
-void test_runner(void (*func)(void), const char *funcname, const char *filename)
+void test_begin(const char *filename)
 {
-	t_test new_test;
+	g_test.filename = filename;
+	g_test.failures = 0;
+	g_test.total = 0;
+	printf("[==========] Running tests for %s\n", filename);
+}
 
-	new_test.funcname = funcname;
-	new_test.filename = filename;
-	new_test.state.failed = 0;
-	g_curr_test = &new_test;
+int test_end(void)
+{
+	printf("[==========]\n");
+	printf("Tests: %d", g_test.total - g_test.failures);
+	printf("\tFailures: %d\n", g_test.failures);
+	if (g_test.failures)
+		return (1);
+	else
+		return (0);
+}
+
+void test_runner(void (*func)(void), const char *funcname)
+{
+	g_test.funcname = funcname;
+	g_test.failed = 0;
+	g_test.total += 1;
 
 	func();
 	display_result();
 }
 
-void display_pass(void)
+void display_func(const char *message)
 {
-	printf("[ %s ] %s", GREEN("PASSED"), g_curr_test->funcname);
+	printf("[  %s  ] %s", message, g_test.funcname);
 }
 
-// Expression Evaluated To FALSE
-// No ASSERTION Found
-void display_fail(void)
+void display_diagnostics()
 {
-	printf("[ %s ] %s", RED("FAILED"), g_curr_test->funcname);
-	printf(" in %s:%d", g_curr_test->filename, g_curr_test->line);
-	printf("%s", RED(" Expression Evaluated To FALSE"));
+	printf(" in %s:%d", g_test.filename, g_test.line);
 }
 
 void display_result(void)
 {
-	if (g_curr_test->state.failed)
-		display_fail();
+	if (g_test.failed)
+	{
+		display_func(TEST_FAIL_MSG);
+		display_diagnostics();
+	}
 	else
-		display_pass();
+		display_func(TEST_PASS_MSG);
 	printf("\n");
 }
--- test_runner.h
+++ test_runner.h
@@ -1,31 +1,34 @@
 #ifndef TEST_RUNNER_H
 # define TEST_RUNNER_H
 
-# define RED(string) "\033[1;31m" string "\033[0m"
-# define GREEN(string) "\033[1;32m" string "\033[0m"
-# define YELLOW(string) "\033[1;33m" string "\033[0m"
-# define BOLD(string) "\033[1m" string "\033[0m"
+# define RED(string)		"\033[1;31m" string "\033[0m"
+# define GREEN(string)		"\033[1;32m" string "\033[0m"
+# define BOLD(string)		"\033[1m" string "\033[0m"
 
-# define RUN(func) test_runner(func, #func, __FILE__)
-# define ASSERT(condition) test_assert(condition, __LINE__)
+# define TEST_FAIL_MSG		RED("FAILED")
+# define TEST_PASS_MSG		GREEN("PASSED")
 
-typedef struct	s_test_state
-{
-	int failed;
-}				t_test_state;
+# define RUN(func)			test_runner(func, #func)
+# define BEGIN()			test_begin(__FILE__)
+# define END()				test_end()
+# define ASSERT(condition)	test_assert(condition, __LINE__)
 
 typedef struct	s_test
 {
-	const char *funcname;
-	const char *filename;
-	int	line;
-	t_test_state state;
+	const char	*funcname;
+	const char	*filename;
+	int			line;
+	int			failed;
+	int			total;
+	int			failures;
 }				t_test;
 
+void test_begin(const char *filename);
+int test_end(void);
 void test_assert(int condition, int line);
-void test_runner(void (*func)(void), const char *funcname, const char *filename);
+void test_runner(void (*func)(void), const char *funcname);
+void display_func(const char *message);
+void display_diagnostics(void);
 void display_result(void);
-void display_fail(void);
-void display_pass(void);
 
 #endif
