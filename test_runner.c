dfcaf9addae50c47fa4005d6a09509e5278fcf3d
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-05-14 22:39:55 -0300

    refatoração

--- test_runner.c
+++ test_runner.c
@@ -1,5 +1,4 @@
 #include "test_runner.h"
-#include <stdio.h>
 
 t_test	g_test;
 
@@ -23,8 +22,18 @@ void	test_runner(t_unit_test *test)
 	test->func();
 }
 
-int		run_grp(t_unit_test tests[], int num_tests, char *filename)
+void	init_tests()
 {
+	if (!g_test.has_entry_point)
+	{
+		g_test.has_entry_point = true;
+		printf("Running unit tests\n");
+	}
+}
+
+bool	run_units(t_unit_test tests[], int num_tests, char *filename)
+{
+	init_tests();
 	int fails = 0;
 	int fail_index[num_tests];
 
@@ -36,19 +45,19 @@ int		run_grp(t_unit_test tests[], int num_tests, char *filename)
 	}
 	if (fails)
 	{
-		printf("%s %s\n", TEST_FAIL_MSG, filename);
+		printf("\n%s %s\n", TEST_FAIL, realpath(filename, NULL));
 		for (int i = 0; i < fails; i++)
 		{
-			printf(" %s %s", FAIL_SYMBOL, tests[fail_index[i]].name);
+			printf(" %s %s", FAIL_SIGN, tests[fail_index[i]].name);
 			printf(" at line %d\n", tests[fail_index[i]].line);
 		}
 	}
 	else
 	{
-		printf("%s %s\n", TEST_PASS_MSG, filename);
+		printf("\n%s %s\n", TEST_PASS, filename);
 		for (int i = 0; i < num_tests; i++)
 		{
-			printf(" %s %s\n", PASS_SYMBOL, tests[i].name);
+			printf(" %s %s\n", PASS_SIGN, tests[i].name);
 		}
 	}
 	return 0;
--- test_runner.h
+++ test_runner.h
@@ -1,22 +1,24 @@
 #ifndef TEST_RUNNER_H
 # define TEST_RUNNER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
 # define BOLD(string)			"\033[1m" string "\033[0m"
 # define BG_RED(string)			"\033[7;31m" string "\033[0m"
 # define FG_RED(string)			"\033[1;31m" string "\033[0m"
 # define BG_GREEN(string)		"\033[7;32m" string "\033[0m"
 # define FG_GREEN(string)		"\033[1;32m" string "\033[0m"
 
-# define TEST_FAIL_MSG			BG_RED(" FAILED ")
-# define TEST_PASS_MSG			BG_GREEN(" PASSED ")
+# define TEST_FAIL				BG_RED(" FAILED ")
+# define TEST_PASS				BG_GREEN(" PASSED ")
 
-# define FAIL_SYMBOL			FG_RED("  ")
-# define PASS_SYMBOL			FG_GREEN(" ✓ ")
+# define FAIL_SIGN				FG_RED("")
+# define PASS_SIGN				FG_GREEN("✓")
 
-# define SIZE_OF_TESTS			sizeof(tests)/sizeof(t_unit_test)
-
-# define run_group(tests)		run_grp(tests, SIZE_OF_TESTS, __FILE__)
-# define testing(FUNC)			{.func=FUNC, .name=#FUNC}
+# define run_group(tests)		run_units(tests, sizeof(tests)/sizeof(tests[0]), __FILE__)
+# define unit_test(function)	{.func=function, .name=#function}
 # define assert_expr(condition)	test_assert(condition, __LINE__)
 
 typedef struct	s_unit_test
@@ -32,10 +34,12 @@ typedef struct	s_test_status
 	t_unit_test *curr;
 	int			total;
 	int			failures;
+	bool		has_entry_point;
 }				t_test;
 
+void	init_tests(void);
 void	test_assert(int condition, int line);
-void	test_runner(t_unit_test *test);
-int		run_grp(t_unit_test tests[], int num_tests, char *filename);
+void	test_runner(t_unit_test *new_test);
+bool	run_units(t_unit_test tests[], int num_tests, char *filename);
 
 #endif
