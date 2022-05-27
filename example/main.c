692230f20268b2c480084c66783a63db700a6ef8
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-05-26 22:43:09 -0300

    modifica código de exemplo

--- example/main.c
+++ example/main.c
@@ -1,17 +1,17 @@
 #include "../test_runner.h"
 #include <string.h>
 
-void	test_func_strcmp(void)
+void	tests_function_that_passes(void)
 {
-	char	*test1 = "palavra";
-	char	*test2 = "palavra1";
+	char	*test1 = "mesma palavra";
+	char	*test2 = "mesma palavra";
 
 	ASSERT_EXPR(strcmp(test1, test2) == 0);
 }
 
 int main() {
 	t_unit_test tests[] = {
-		UNIT_TEST(test_func_strcmp),
+		UNIT_TEST(tests_function_that_passes),
 	};
 	RUN_GROUP(tests);
 }
